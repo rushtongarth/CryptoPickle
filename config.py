5c61b02343b9e92d380d4f34433512c6682573ee
rushton <rushtongarth@users.noreply.github.com>
2016-05-26 15:55:27 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os,getpass
 
 thisdir = os.path.expanduser("~")+os.sep+'.gnupg'
 thisusr = os.getenv("USER")
-stdname = 'keys.cfg'
+stdname = os.path.join(os.getcwd(),'keys.cfg')
 
 class storageinfo(object):
 	"""
@@ -10,18 +10,14 @@ class storageinfo(object):
 	used by CryptoPickle
 	Typical Arguments
 		keyuser=<owner of the key>
-		home=<where your gnupg keys are>
-		fname=<name of the file that will be en/decrypted>
-		loc=<where is the file that is going to be en/decrypted>
+		home=<where your gnupg home>
+		loc=<full path to file that is going to be en/decrypted>
 	"""
 	def __init__(self,**kwargs):
-		self.kw = kwargs
-		self.p = None
-		self.u = self.kw.get('keyuser',thisusr)
-		self.h = self.kw.get('home',thisdir)
-		self.fname = self.kw.get('fname',stdname)
-		l = self.kw.get('loc','')
-		self.keyloc = l+os.sep if len(l) else l
+		self.u = kwargs.get('keyuser',thisusr)
+		self.h = kwargs.get('home',thisdir)
+		f = kwargs.get('loc',stdname)
+		self.keydir,self.keyfile=os.path.split(f)
 
 	def __call__(self,**kwargs):
 		info = storageinfo(**kwargs)
@@ -30,5 +26,8 @@ class storageinfo(object):
 		return info
 
 	def getcryptname(self):
-		tmp = self.fname.split('.')
-		return self.keyloc+'.'.join(i for i in tmp[:-1])+'.crypto.'+tmp[-1]
+		if not 'crypto' in self.keyfile:
+			tmp = self.keyfile.rsplit('.',1)
+			tmp.insert(1,'crypto')
+			self.keyfile = '.'.join(tmp)
+		return os.path.join(self.keydir,self.keyfile)
